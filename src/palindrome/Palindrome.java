a79fb6346c2863d77e63f59015d0669160f1ce08
evrob0095 <evrob0095@7L9N6Y1.Student.UGDSB.ED>
2020-02-20 13:58:50 -0500

    2nd commit

--- src/palindrome/Palindrome.java
+++ src/palindrome/Palindrome.java
@@ -1,7 +1,8 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Evan Robertson
+ * February 20 2020
+ * Palindrome.java
+ * This program revesres a word to see if it is the same
  */
 
 package palindrome;
@@ -18,15 +19,20 @@ public class Palindrome {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        //Get input
         String palindrome = JOptionPane.showInputDialog("Enter a word to check");
+        //make array to get all characters of string
         char[] arr = new char[palindrome.length()];
+        //Turn string into array
         for(int i = 0; i < palindrome.length(); i++){
             arr[i] = palindrome.charAt(i);
         }
+        //Rewrite backwards
         String newString = "";
         for(int i = 0; i < palindrome.length(); i++){
             newString = arr[i] + newString;
         }
+        //
         if (palindrome.equals(newString)) {
             JOptionPane.showMessageDialog(null, "Your word: " + palindrome + ", "
                     + "is the same when reversed and is therefore a palindrome");
